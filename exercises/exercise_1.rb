70e84b3a7b74a0ce26c63caa166b514463fd0e80
Arturas Zuta <arturaszuta@gmail.com>
2019-09-16 13:47:02 -0400

    Add 3 more stores, and variables for mens and womens stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 # Your code goes below here ...
 
 class Store < ActiveRecord::Base
-  attr_accessor :store1, :store2
+  attr_accessor :store1, :store2, :store3, :mens_stores, :womens_stores
 end
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true);
@@ -15,11 +15,38 @@ Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, wom
 
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
 
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true);
+
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false);
+
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true);
+
 @store1 = Store.find(1)
 @store2 = Store.find(2)
+@store3 = Store.find(3)
 
 @store1.update(name: 'Dollarama')
 p @store1
+@store3.destroy()
+
+p Store.count()
+
+@mens_stores = Store.where("mens_apparel = true")
+
+@mens_stores.each { |e|
+  puts "Name of the store: #{e.name}"
+  puts "Revenue of this store: #{e.annual_revenue}"
+  puts "-----------------------------"
+}
+
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000");
+
+p @womens_stores
+
+
+
+
 
 
 
